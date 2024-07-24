1e1223133824a7859aa56f8b15b23014c03bf429
Correa95 <mathewcorrea19@gmail.com>
2024-07-23 21:22:06 -0400

    connect the navbar to all pages

---
+++ script.js
@@ -0,0 +1,19 @@
+// const numCow = 50;
+
+// const firstCode = "Hello World!";
+// console.log(firstCode);
+
+// const str = "Hello " + " Disney Land";
+// console.log(str);
+
+// const numUmbrella = 10 * 3;
+
+// const num = 5 + 1;
+// console.log(num);
+
+// const concatStr = "Hell " + "Space " + "World.";
+// console.log(concatStr);
+
+// const concat = "Hello, " + "space " + "I am " + "space " + "Josh";
+// console.log(concat);
+// alert(concat);
